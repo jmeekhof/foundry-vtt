24069e29b8fd1b4708ff7402f578b1f3d71822ec
Josh Meekhof <jmeekhof@twotheleft.com>
2020-12-21 12:36:23 -0500

    Add key pair to configuration

---
+++ keys.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "foundry_key" {
+  key_name   = "foundry_key"
+  public_key = "file(${var.PATH_TO_PUBLIC_KEY})"
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "region" {
   default = "us-east-1"
 }
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/foundry-vtt.pem"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/foundry-vtt.pub"
+}
